24f82deb7d8f6ee4b817f9b39892437c547d88a2
Belajar Git <ahmad0sucipto@student.uns.ac.id>
2020-07-08 19:04:03 +0700

    last update

--- views/site/index.php
+++ views/site/index.php
@@ -12,14 +12,9 @@ $period_link = "$self_link?r=periode%2Findex";
     <div class="jumbotron" style="background-image: url(images/image.jpg)";>
         <h1>Koperasi</h1>
 
-        <p class="lead">You have successfully created your Yii-powered application.</p>
-
         <p><a class="btn btn-lg btn-success" href=<?= $member_link?>>Daftar Anggota</a></p>
         
         <p><a class="btn btn-lg btn-success" href=<?= $period_link?>>Daftar Periode</a></p>
     </div>
-
-    <div class="body-content">
-
-    </div>
+    
 </div>
